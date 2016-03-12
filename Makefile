b98759c2446a83d5cbb05f5caba6631d5fbea2bb
hwlin <hwlin1414@gmail.com>
2016-03-12 20:38:00 +0800

    modify rc script

--- Makefile
+++ Makefile
@@ -7,10 +7,11 @@ SRCS=			src/main.c
 LDADD=			-lpthread
 MAKEOBJDIR=
 MAN=
+CC=clang
 
 BINDIR=			/usr/local/sbin
 BINMODE=		0755
-FILES=			etc/judge.conf
+FILES=			etc/bsdjudge.conf
 FILESDIR=		/usr/local/etc
 FILESMODE=		0644
 SCRIPTS=		etc/rc.d/bsdjudge
--- src/main.c
+++ src/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+void sighand(int signo){
+    if(signo == SIGTERM){
+        exit(0);
+    }else if(signo == SIGHUP){
+        printf("reloading...\n");
+    }else{
+        printf("signal recieve: %s\n", sys_signame[signo]);
+    }
+}
 
 int main(int argc, char **argv){
-    printf("hello judge\n");
+    int i, pid;
+    FILE *pidfile = NULL;
+    if(fork() == 0){
+        pid = fork();
+        if(pid == 0){
+            for(i = 0; i < 100; i++) signal(i, sighand);
+            while(1){
+                sleep(1);
+            }
+        }
+        pidfile = fopen("/var/run/bsdjudge.pid", "w");
+        if(pidfile != NULL){
+            fprintf(pidfile, "%d\n", pid);
+            fclose(pidfile);
+        }
+        exit(0);
+    }
+    wait(NULL);
     return 0;
 }
